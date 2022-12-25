fab1576d6a0aefe11435f95add6f6a28426c80f1
pashakar <pashakart961@gmail.com>
2022-12-25 14:58:36 +0700

    added ValidationPipe

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,21 @@
+import { IsString, IsNumber, Max, Min } from 'class-validator';
+
 export class ReviewDto {
+
+  @IsString()
   name: string;
+
+  @IsString()
   title: string;
+
+  @IsString()
   description: string;
+
+  @IsNumber()
+  @Max(5)
+  @Min(1)
   rating: number;
+
+  @IsString()
   productId: string;
 }
--- src/review/review.controller.ts
+++ src/review/review.controller.ts
@@ -7,6 +7,8 @@ import {
   HttpStatus,
   Param,
   Post,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { ReviewDto } from './dto/create-review.dto';
 import { REVIEW_NOT_FOUNDED } from './review.constant';
@@ -16,6 +18,7 @@ import { ReviewService } from './review.service';
 export class ReviewController {
   constructor(private readonly reviewService: ReviewService) {}
 
+  @UsePipes(new ValidationPipe())
   @Post('create')
   async create(@Body() dto: ReviewDto) {
     return this.reviewService.create(dto);
